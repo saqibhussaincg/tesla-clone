ceabcbe08d31bebe0b1516c7dbac6e979c98b4d5
saqib <saqibhussaincg@gmail.com>
2022-03-30 12:40:13 +0500

    changes

---
+++ src/app/components/Home.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home() {
+  return <div>
+    <Container>
+        <Section 
+            title="Model S"
+            description= "Order Online For Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+         <Section 
+            title="Model Y"
+            description= "Order Online For Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Model 3"
+            description= "Order Online For Touchless Delivery"
+            backgroundImg="model-3.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Model X"
+            description= "Order Online For Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Lowest Cost Solar Panel In America"
+            description= "Money Back Guarantee"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+        />
+        <Section 
+            title="Solar For New Roofs"
+            description= "oney Back Guarantee"
+            backgroundImg="solar-roof.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Inventory"
+        />
+        <Section 
+            title="Accessories"
+            description= ""
+            backgroundImg="accessories.jpg"
+            leftBtnText="Custom Order"
+        />
+    </Container>
+  </div>;
+}
+
+export default Home;
+
+const Container = styled.div`
+    height: 100vh;
+`
